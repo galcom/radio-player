49bf59e9434d63f33a31da5a81da070bbabfc8a2
Andrew Alkema <andrew.alkema@d2l.com>
2019-10-26 16:43:54 -0400

    Add layout

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,39 +1,34 @@
 import React from 'react';
 import './Player.css';
 import FilePlayer from 'react-player/lib/players/FilePlayer'
-import { Container, Row, Col } from 'reactstrap';
+import { Image } from 'react-bootstrap'
 
 class Player extends React.Component {
     render() {
         return (
-            <Container fluid>
-                <Row>
-                    <Col>
-                        <img src='FaithTechRadio.png' alt="logo" className="fit-image" />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <img
-                            src={this.props.playControlImage + ".gif"}
-                            alt="play/pause"
-                            onClick={() => { this.props.togglePlaying() }}
-                            fluid
-                        />
-                        <FilePlayer
-                            url={this.props.streamUrl} //http://xstreamer.galcom.org:8000/GalcomCanada
-                            playing={this.props.playing}
-                            config={{
-                                file: {
-                                    forceAudio: true,
-                                    autoplay: true
-                                }
-                            }}
-                            onPlay={() => { this.props.onStartPlaying() }}
-                        />
-                    </Col>
-                </Row>
-            </Container>
+            <div id="wrapper">
+                <div id="logo"><Image src='FaithTechRadio.png' alt="logo" fluid /></div>
+                <div id="control">
+                    <Image
+                        src={this.props.playControlImage + ".gif"}
+                        alt="play/pause"
+                        onClick={() => { this.props.togglePlaying() }}
+                        className="controlButton"
+                        fluid
+                    />
+                    <FilePlayer
+                        url={this.props.streamUrl} //http://xstreamer.galcom.org:8000/GalcomCanada
+                        playing={this.props.playing}
+                        config={{
+                            file: {
+                                forceAudio: true,
+                                autoplay: true
+                            }
+                        }}
+                        onPlay={() => { this.props.onStartPlaying() }}
+                    />
+                </div>
+            </div>
         )
     }
 }
