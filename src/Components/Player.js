46ec80685d85d1b5d95ad5c31a63cbcde56df49e
Kevin Willms <kwillms@desire2learn.com>
2019-10-26 10:59:04 -0400

    Added React Player

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -4,7 +4,12 @@ import ReactPlayer from 'react-player'
 
 
 function Player(props) {
-  return <ReactPlayer url='http://xstreamer.galcom.org:8000/GalcomCanada' playing controls="true"/>
+  return (
+  <div class='PlayerWindow'>
+    <ReactPlayer url='http://xstreamer.galcom.org:8000/GalcomCanada' playing controls="true" width='100%' height='100%'/>
+  </div>
+);
 }
+
 
 export default Player;
