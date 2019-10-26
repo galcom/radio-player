dda84707684fb98d3df86cf75c92e428df24d1da
Andrew Alkema <andrew.alkema@d2l.com>
2019-10-26 11:32:31 -0400

    add_playback_toggle

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './Player.css';
 import ReactPlayer from 'react-player'
 
-
-function Player(props) {
-  return (
-  <div class='PlayerWindow'>
-    <ReactPlayer url='http://xstreamer.galcom.org:8000/GalcomCanada' playing controls="true" width='100%' height='100%'/>
-  </div>
-);
+class Player extends React.Component {
+    render() {
+        return (
+            <div class='PlayerWindow'>
+                <input onClick={() => { this.props.togglePlaying() }} value=">" />
+                <ReactPlayer
+                    url='http://xstreamer.galcom.org:8000/GalcomCanada'
+                    playing={this.props.playing}
+                    width='100%'
+                    height='100%'
+                />
+            </div>
+        )
+    }
 }
 
 
