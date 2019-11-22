74cfcc4d38658eeca380d0db9e9ceae7f9963a55
Ben Fulton <fulton.benjamin@gmail.com>
2019-11-21 20:14:00 -0500

    Update radio colors

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -4,38 +4,50 @@ import FilePlayer from 'react-player/lib/players/FilePlayer'
 
 
 class Player extends React.Component {
-    render() {
-        return (
-            <div id="wrapper">
-                <div id="top">
-                  <img
-                    src={this.props.logo}
-                    alt="logo"
-                    id="logo"
-                  />
-                </div>
-                <div id="bottom">
-                    <img
-                      src={this.props.playControlImage + ".gif"}
-                      alt="play/pause"
-                      onClick={() => { this.props.togglePlaying() }}
-                      id="control"
-                    />
-                    <FilePlayer
-                        url={this.props.streamUrl}
-                        playing={this.props.playing}
-                        config={{
-                          file: {
-                            forceAudio: true,
-                            autoplay: true
-                          }
-                        }}
-                        onPlay={() => { this.props.onStartPlaying() }}
-                    />
-                </div>
-            </div>
-        )
+  render() {
+    // create background & foreground color styles
+    const bgColorStyle = {
+      backgroundColor: this.props.backgroundColor,
     }
+    const fgColorStyle = {
+      foregroundColor: this.props.foregroundColor,
+    }
+
+    return (
+      <div id="wrapper">
+        <div id="top" style={bgColorStyle}>
+          <img
+            src={this.props.logo}
+            alt="logo"
+            id="logo"
+          />
+        </div>
+        <div id="bottom" style={bgColorStyle}>
+            <img
+              id="control"
+              src={this.props.playControlImage + ".gif"}
+              alt="play/pause"
+              onClick={() => {
+                this.props.togglePlaying()
+              }}
+            />
+            <FilePlayer
+                url={this.props.streamUrl}
+                playing={this.props.playing}
+                config={{
+                  file: {
+                    forceAudio: true,
+                    autoplay: true
+                  }
+                }}
+                onPlay={() => {
+                  this.props.onStartPlaying()
+                }}
+            />
+        </div>
+      </div>
+    )
+  }
 }
 
 
